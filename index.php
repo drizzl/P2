9f6650026562e947fc4ff04b218283507bcd03c9
Dan Runt <dan.runt@gmail.com>
2016-09-21 19:56:36 -0400

    bug fix, added features, formatting

--- index.php
+++ index.php
@@ -6,27 +6,8 @@
     <?php require 'logic.php'; ?>
 
     <style>
-        .error {
-            background-color:red;
-            color:white;
-            padding:5px;
-            width:300px;
-            margin:5px 0px;
-        }
-        section {
-            margin:25px 0px;
-        }
-        label {
-            display:block;
-        }
-		.invisible {
-			background-color:transparent;
-			border: 0px solid;
-			outline: none;
-			box-shadow: none;
-			height: 30px;
-			width: 260px;
-			vertical-aligh: middle;
+		.block {
+			display: block;
 		}
     </style>
 	
@@ -43,14 +24,27 @@
 
     <form method='POST' action='index.php'>
 	
-		<?php if (isset($nw_error)||isset($db_error)): ?>
+		<?php if (isset($nw_error)||isset($db_error)||isset($val_error)): ?>
 		<p style="color:red">Fields in red are invalid</p>
 		<?php endif; ?>
 
-		<p style="color:<?php echo ($nw_error ? "red":"black") ?>">Number of words in password <input type='number' name='num_words' value="<?php echo $num_words?>"> (1-9)</p>
-		<p style="color:<?php echo ($db_error ? "red":"black") ?>">Number of words in database <input type='number' name='database_size' value="<?php echo $database_size?>">/5000 most commonly used words</p>
-        Use numbers <input type='checkbox' name='numbers' <?php if (isset($numbers)) echo "checked"?>><br>
-        Use symbols <input type='checkbox' name='symbols' <?php if (isset($symbols)) echo "checked"?>><br>
+		<p style="color:<?php echo (isset($nw_error) ? "red":"black") ?>">Number of words in password <input type='number' name='num_words' value="<?php echo $num_words?>"> (1-9)</p>
+		<p style="color:<?php echo (isset($db_error) ? "red":"black") ?>">Number of words in database <input type='number' name='database_size' value="<?php echo $database_size?>">/5000 most commonly used words</p>
+		<?php if (isset($val_error)) echo '<p style="color:red"> Database must be bigger than number of words!</p>'?>
+        <p> 
+			Include Numbers <br>
+				<label class="block"> <input type='radio' name='numbers' value="No" <?php if (!isset($numbers)||$numbers=="No") echo "checked"?> >No</label>
+				<label class="block"> <input type='radio' name='numbers' value="Starting" <?php if (isset($numbers)&&$numbers=="Starting") echo "checked"?> >Starting</label>
+				<label class="block"> <input type='radio' name='numbers' value="Ending" <?php if (isset($numbers)&&$numbers=="Ending") echo "checked"?> >Ending</label>
+				<label class="block"> <input type='radio' name='numbers' value="Between" <?php if (isset($numbers)&&$numbers=="Between") echo "checked"?> >Between Words</label>
+		</p>
+        <p> 
+			Use symbols <br>
+				<label class="block"> <input type='radio' name='symbols' value="No" <?php if (!isset($symbols)||$symbols=="No") echo "checked"?> >No</label>
+				<label class="block"> <input type='radio' name='symbols' value="Starting" <?php if (isset($symbols)&&$symbols=="Starting") echo "checked"?> >Starting</label>
+				<label class="block"> <input type='radio' name='symbols' value="Ending" <?php if (isset($symbols)&&$symbols=="Ending") echo "checked"?> >Ending</label>
+				<label class="block"> <input type='radio' name='symbols' value="Between" <?php if (isset($symbols)&&$symbols=="Between") echo "checked"?> >Between Words</label>
+		</p>
 
         <input type='submit' value='Generate!'>
 
--- logic.php
+++ logic.php
@@ -3,9 +3,9 @@
 $dictionary = [];
 
 #Initialize symbol array with static data
-$symbol_arr = array('`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', 
-				 '_', '+', '=', '\\', '|', ']', '}', '{', '[', '"', "'", ';', ':', 
-				 ',', '<', '>', '.', '/', '?');
+$symbol_arr = array('`', '~', '!', '@', '#', '$', '%', '^', '&amp', '*', '(', ')', '-', 
+				 '_', '+', '=', '\\', '|', ']', '}', '{', '[', '&quot', "&apos", ';', ':', 
+				 ',', '&lt', '&gt', '.', '/', '?');
 
 #This code section checks to see if the dictionary has been loaded from
 # the text file yet. If not, load it from the file and store on _POST variable.
@@ -29,8 +29,8 @@ if (isset($_POST["num_words"])) {
 	#set the variables, so that they default after post
 	$num_words = $_POST["num_words"];
 	$database_size = $_POST["database_size"];
-	if (isset($_POST["numbers"])) $numbers = 1;
-	if (isset($_POST["symbols"])) $symbols = 1;
+	if (isset($_POST["numbers"])) $numbers = $_POST["numbers"];
+	if (isset($_POST["symbols"])) $symbols = $_POST["symbols"];
 
 	#Now we check to see if the number of words is between 0 and 10, otherwise *error*
 	if ($num_words <= 0 || $num_words > 9) {
@@ -43,35 +43,49 @@ if (isset($_POST["num_words"])) {
 		if ($database_size <= 0 || $database_size > 5000) {
 			$db_error = 1;
 		}
+		if ($database_size < $num_words) {
+			$val_error = 1;
+		}
 	} else {
 		#Now we check to see if the database size is valid.
 		if ($database_size <= 0 || $database_size > 5000) {
 			$db_error = 1;
 		} else {
-			#Now we take only the requested number of words out of the "database"
-			$password_arr = [];
-			for ($i = 0; $i<$database_size; $i++) {
-				$password_arr[$i] = $dictionary[$i];
+			if ($database_size < $num_words) {
+				$val_error = 1;
+			} else {
+				#Now we take only the requested number of words out of the "database"
+				$password_arr = [];
+				for ($i = 0; $i<$database_size; $i++) {
+					$password_arr[$i] = $dictionary[$i];
+				}
+
+				#$password_arr now contains the requested word pool. We shuffle them to randomize.
+				shuffle($password_arr);
+
+				$password = '';
+				#if they want to start with numbers and symbols, add them now
+				if (isset($numbers)&&$numbers=="Starting") $password .= rand(0,99);
+				if (isset($symbols)&&$symbols=="Starting") $password .= $symbol_arr[rand(0,31)];
+
+				$word_count=0;
+				#Now we only keep the first X words to create the password.
+				foreach($password_arr as $value) {
+					
+					#ucfirst will capitalize the first letter, to make it more readable
+					$password .= ucfirst($value);
+
+					$word_count++;
+					if ($word_count==$num_words) break; #if we have the whole password, exit.
+					
+					if (isset($numbers)&&$numbers=="Between") $password .= rand(0,99);
+					if (isset($symbols)&&$symbols=="Between") $password .= $symbol_arr[rand(0,31)];
+				}
+
+				#add random numbers and symbols if requested
+				if (isset($numbers)&&$numbers=="Ending") $password .= rand(0, 99);
+				if (isset($symbols)&&$symbols=="Ending") $password .= $symbol_arr[rand(0, 31)];
 			}
-
-			#$password_arr now contains the requested word pool. We shuffle them to randomize.
-			shuffle($password_arr);
-
-			$password = '';
-			$word_count=0;
-			#Now we only keep the first X words to create the password.
-			foreach($password_arr as $value) {
-				
-				#ucfirst will capitalize the first letter, to make it more readable
-				$password .= ucfirst($value);
-
-				$word_count++;
-				if ($word_count==$num_words) break; #if we have the whole password, exit.
-			}
-
-			#add random numbers and symbols if requested
-			if (isset($numbers)) $password .= rand(0, 99);
-			if (isset($symbols)) $password .= $symbol_arr[rand(0, 31)];
 		}
 	}
 }
